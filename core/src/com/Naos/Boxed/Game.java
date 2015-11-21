d276f924c9175e7f2b6e204af6d5f1716d33eebe
daimon_sewell <daimon.sewell@rbgapps.com>
2015-11-21 06:26:01 +0200

    Boxed 2 - Box generator style Vertical Added Line impact detector to Box generator (Supply line graph in X,Y origins and gradient)

--- core/src/com/Naos/Boxed/Game.java
+++ core/src/com/Naos/Boxed/Game.java
@@ -34,9 +34,9 @@ public class Game extends Screen {
     public Boolean Create(DeltaBatch batch) {
         CreateAgain(true);
         if (playertype == null) playertype = playertype.normal;
-        r = new Rect(20*fXunit,54*fYunit,(20*fXunit) + 8*fYunit,46*fYunit);
+        r = new Rect(45*fXunit,(10*fYunit) + (10*fXunit),(55*fXunit),(10*fYunit));
         spPlayer = playertype.getSpriteFromRect(r);
-        bxGen = new BoxGenerator(1,10,(int)(100*fXunit));
+        bxGen = new BoxGenerator((int)(0.5f*fYunit),10,(int)(100*fYunit));
         return super.Create(batch);
     }
 
@@ -54,6 +54,13 @@ public class Game extends Screen {
         spPlayer.draw(batch.batch);
         bxGen.Update();
         bxGen.Draw(batch);
+
+        if (rFullscreen.IsTouched()) {
+            float fX = rFullscreen.TouchedX();
+            float fY = rFullscreen.TouchedY();
+            float fGradient = (float)/*Math.toDegrees*/(/*Math.atan*/(((fY - spPlayer.getY())/(fX - spPlayer.getX()))));
+            bxGen.LineImpact(spPlayer.getX(),spPlayer.getY(),fGradient);
+        }
         return super.Draw(batch);
     }
 
@@ -74,25 +81,36 @@ public class Game extends Screen {
         List<Box> BOXES = new ArrayList<Box>();
         List<Integer> iSpawnPoints = new ArrayList<Integer>();
         int iNumBoxTypes = 1;
-        int iDivisions = 10;
-        int iMetre = (int)(100*fYunit) / (iDivisions + 4);
-        int iStartY = 2 * iMetre;
+        int iDivisions = 15;
+        int iMetre = (int)((100*fXunit) / (iDivisions));
+        int iStartY = 0;
         int iStartX = 0;
         int iSpaceTimer = 0;
 
-        public BoxGenerator(int iSpeed, int iCrowdedness,int fStartX) {
+        public BoxGenerator(int iSpeed, int iCrowdedness,int fStartY) {
             this.iSpeed = iSpeed;
-            this.iStartX = fStartX;
+            this.iStartY = fStartY;
             this.iCrowdedness = iCrowdedness;
-            for (int i = 0; i < 10 ; i++) {
+            for (int i = 0; i < iDivisions ; i++) {
                 iSpawnPoints.add(0);
             }
+        }
+
+        public float[] LineImpact(float fOriginX, float fOriginY,float fAngle) {
+            for (Box b: BOXES) {
+                float[] fImpact = b.Impact(fOriginX,fOriginY,fAngle);
+                if (fImpact[0] >= 0) {
+                    return fImpact;
+                }
+            }
+            float[] f = {-1,-1};
+            return f;
         }
 
         public void Update() {
             boolean bUpdatePoints = false;
             for (Box b : BOXES) {
-                b.translateX(-iSpeed);
+                b.translateY(-iSpeed);
             }
             if (iSpaceTimer > 0) {
                 iSpaceTimer -= iSpeed;
@@ -104,29 +122,29 @@ public class Game extends Screen {
                 // Work through the spawnpoints form the bottom up
                 int iWaitTime = iSpawnPoints.get(i);
                 if (iWaitTime == 0) { // Need to spawn boxes
-                    int iVertSpace = 1;
+                    int iHorSpace = 1;
                     // Find out how much space you have going up
                     for (int j = i + 1; j < iSpawnPoints.size();j++) {
                         if (iSpawnPoints.get(j) != 0) {
                             break;
                         } else {
-                            iVertSpace++;
+                            iHorSpace++;
                         }
                     } // Found it
 
                     Box b = null;
                     int iBoxType = new Random().nextInt(iNumBoxTypes) + 1;
-                    int iXSize = new Random().nextInt(iVertSpace) + 1;
-                    int iYsize = new Random().nextInt(iVertSpace) + 1;
+                    int iXSize = new Random().nextInt(iHorSpace) + 1;
+                    int iYsize = new Random().nextInt(iHorSpace) + 1;
                     switch (iBoxType) {
                         case 1:
-                            b = new Box(imgBox1,iMetre,iXSize,iYsize,iStartX,iStartY + (i*iMetre));
+                            b = new Box(imgBox1,iMetre,iXSize,iYsize,iStartX+ (i*iMetre),iStartY );
                             iXSize = Math.min(iXSize,b.MaxSize());
                             iYsize = Math.min(iYsize,b.MaxSize());
                     }
                     BOXES.add(b);
-                    for (int k = i; k < i + iYsize;k++) {
-                        iSpawnPoints.set(k, iXSize);
+                    for (int k = i; k < i + iXSize;k++) {
+                        iSpawnPoints.set(k, iYsize);
                     }
                 } else if (bUpdatePoints){
                     iSpawnPoints.set(i,iWaitTime-1);
@@ -143,6 +161,23 @@ public class Game extends Screen {
         class Box extends Sprite {
             int iXsize, iYsize;
             float fMetre;
+            boolean bHit = false;
+
+            public float[] Impact(float fOriginX, float fOriginY, float fAngle) {
+                Rect r = new Rect(getX(),getY() + getHeight(),getX() + getWidth(), getY());
+                float fC = fOriginY - (fOriginX * (float)fAngle);
+                float fXPos = (float)((r.b() - fC)/(fAngle));
+                if (fXPos > r.l() && fXPos < r.r() && !bHit) {
+                    float[] f = {fXPos,r.b()};
+                    setAlpha(0.1f);
+                    bHit = true;
+                    return f;
+                } else {
+                    float[] f = {-1,-1};
+                    return f;
+                }
+            }
+
             Box(Texture tx, float fMetre,int iXSize, int iYsize, float fStartPointx, float fStartPointy) {
                 super(tx);
                 this.fMetre = fMetre;
--- desktop/src/com/Naos/Boxed/desktop/DesktopLauncher.java
+++ desktop/src/com/Naos/Boxed/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.Naos.Boxed.BoxedMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.height = 800;
+        config.width = 400;
 		new LwjglApplication(new BoxedMain(), config);
 	}
 }
